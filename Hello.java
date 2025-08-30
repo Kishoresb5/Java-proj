161384872ee120f5dd1bee2f6d8281f3eb9caf62
ubuntu <ubuntu@ip-172-31-21-236.eu-west-2.compute.internal>
2025-08-30 14:36:55 +0000

    six commit

--- Hello.java
+++ Hello.java
@@ -1,9 +1,8 @@
 //your first program
 //
 
-class hello {
-        public static void main(String[]args) {
+class Hello {
+        Public static void main(String[]args) {
           System.out.println("Welcome to Git practice");
         }
 }
-
