75ab30549841653ea4dffa899223a7738d743de9
Kishoresb5 <kishoresb52@gmail.com>
2025-08-31 13:49:08 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@
 
 class Hello {
         public static void main(String[]args) {
-          System.out.println("Welcome to Git practice");
+          System.out.println("Welcome to Github");
         }
 }
